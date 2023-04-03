7095d7630255568cbfbc9b30adc9a2c8e41bcf15
Lisa Munsy <stillailleurs@gmail.com>
2023-04-03 10:13:08 +0200

    Added a bit of style to the page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,16 +9,29 @@
 
     <div class="l10n-examples">
       <div class="labels">
+        <h3>Labels</h3>
         Hello
       </div>
       <div class="dates">
+        <h3>Dates</h3>
         Today's date : {{ Date.now() }}
       </div>
       <div class="currency">
-        
+        <h3>Currencies</h3>
       </div>
     </div>
   </main>
 </template>
 <style>
+header{
+  padding: 1rem;
+  color: white;
+  background-color: maroon;
+}
+h3{
+  font-size: 1.5rem;
+}
+.labels, .dates, .currency{
+  margin-bottom: 2rem;
+}
 </style>
