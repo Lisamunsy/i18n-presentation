2427c63ff0d86722cee5f5d60208f571f95b2de4
Lisa Munsy <stillailleurs@gmail.com>
2023-04-03 09:47:30 +0200

    Setup homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <header>
+    <h1 > I18n Worshop</h1>
+  </header>
+  <main class="container">
+
+    <div class="l10n-examples">
+      <div class="labels">
+        Hello
+      </div>
+      <div class="dates">
+        Today's date : {{ Date.now() }}
+      </div>
+      <div class="currency">
+        
+      </div>
+    </div>
   </main>
 </template>
+<style>
+</style>
